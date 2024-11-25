675bbd27371efe2003992f4ab14e1364dd39f6c1
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-11-25 06:25:15 -0300

    feat: add spring project

---
+++ taskify-api/src/test/java/com/gmalheiro/tasks/taskify_api/TaskifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmalheiro.tasks.taskify_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
